23ce7aa2fbdf19cb99673913642cd8bee988d484
Taylor Lopez <taylor0lopez@gmail.com>
2021-03-09 12:10:08 -0700

    Adding other compendium variations from same repo.

--- test/test_item_xml.py
+++ test/test_item_xml.py
@@ -3,8 +3,8 @@ import re
 
 from model.data_loader import DataLoader
 
-dl = DataLoader('data/xml/Complete.xml')
+dl = DataLoader('data/xml/CoreOnly.xml')
 
 # prints memory usage in mb
-# import os, psutil
-# print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+import os, psutil
+print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
