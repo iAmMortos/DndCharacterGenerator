5b86619d24131b0793b77cfedd4c3ed23c2b62ee
Taylor Lopez <taylor0lopez@gmail.com>
2021-09-08 10:59:29 -0600

    Added NPC Name Generator

---
+++ utils/npc_name_generator.py
@@ -0,0 +1,45 @@
+
+import random
+
+
+class NPCNameGenerator(object):
+  def __init__(self):
+    with open('data/npc_names.csv') as f:
+      data = [line.strip().split(',') for line in f.readlines()]
+    self._first_names = []
+    self._epithets = []
+    self._nouns = []
+    for datum in data:
+      a,b,c,d,e = datum
+      self._first_names += [a]
+      self._epithets += [b, d]
+      self._nouns += [c, e]
+
+  def generate_random_name(self):
+    first = random.choice(self._first_names)
+    epithet = random.choice(self._epithets)
+    noun = random.choice(self._nouns)
+
+    return f'{first} {epithet}{noun}'
+
+  def generate_paired_name(self):
+    first = random.choice(self._first_names)
+    i = random.randint(0, len(self._nouns) - 1)
+    last = f'{self._epithets[i]}{self._nouns[i]}'
+    return f'{first} {last}'
+
+
+if __name__ == '__main__':
+  import os
+  import sys
+  sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+  os.chdir('..')
+
+  gen = NPCNameGenerator()
+  print("Randomized Last Names:")
+  for _ in range(10):
+    print('  ', gen.generate_random_name())
+
+  print("\nPre-Paired Last Names:")
+  for _ in range(10):
+    print('  ', gen.generate_paired_name())
