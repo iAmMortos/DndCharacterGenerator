fd8076ffa032c2e60cd87e1e297d4df21a4d7510
Mortos Der Soulstealer <iAmMortos@users.noreply.github.com>
2023-12-29 14:54:25 -0700

    sync

--- templater/templater.py
+++ templater/templater.py
@@ -1,5 +1,4 @@
 
-from templater.utils.properties_file import PropertiesFile
 from templater.template_manager import TemplateManager
 from templater.template_output_types import TemplateOutputTypes
 
@@ -8,19 +7,6 @@ class Templater (object):
   def __init__(self, output_type, config_path='templater/config/application.properties'):
     self.output_type = TemplateOutputTypes(output_type)
     self.template_manager = TemplateManager(self.output_type)
-    self.properties = PropertiesFile(config_path)
-
-  @property
-  def _token_start(self):
-    return self.properties.get('token_start')
-
-  @property
-  def _token_end(self):
-    return self.properties.get('token_end')
-
-  @property
-  def _token_delimiter(self):
-    return self.properties.get('token_delimiter')
 
   def make(self, o):
     pass
---
+++ templater/token.py
@@ -0,0 +1,7 @@
+
+
+class Token (object):
+  def __init__(self, value, *flags):
+    self.value = value
+    self.flags = flags
+  
